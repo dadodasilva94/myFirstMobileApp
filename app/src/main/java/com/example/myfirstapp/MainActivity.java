2e5a5f722ec4453c170fee7dfd7bb50776faa1e0
Paulo Eduardo <dado@pdsolucoes.com.br>
2021-06-03 21:51:00 -0300

    feat: Creating a listView and add button

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
+        List<String> alunos = new ArrayList<>(Arrays.asList("Raissa", "Dado", "Joao", "Vinicius"));
+
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
+
 }
